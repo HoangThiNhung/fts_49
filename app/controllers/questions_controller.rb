9b459adb05b49514f51c05d496e0f8f5010f1455
HoangNhung <hoang.thi.nhung@framgia.com>
2016-01-06 14:27:14 +0700

    user update and delete question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,13 @@
 class QuestionsController < ApplicationController
-  before_action :load_subjects, only: [:index, :new, :create]
-  before_action :correct_user, only: :show
+  before_action :load_subjects, except: [:index, :destroy]
   before_action :authenticate_user!
+  load_and_authorize_resource
   
   def index
     @questions = current_user.questions.page(params[:page]).per 5
   end
 
   def show
-    @question = Question.find params[:id]
     @options = @question.options
   end
 
@@ -24,8 +23,25 @@ class QuestionsController < ApplicationController
       flash[:success] = t "question.notice.create_success"
       redirect_to questions_path
     else
-      render "new"
+      render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update_attributes question_params
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+    flash[:success] = t "question.notice.delete_success"
+    redirect_to questions_path
   end
 
   private
@@ -36,10 +52,5 @@ class QuestionsController < ApplicationController
   def question_params
     params.require(:question).permit :content, :subject_id, :question_type, 
      options_attributes: [:id, :content, :is_correct]
-  end
-
-  def correct_user
-    @question = current_user.questions.find_by id: params[:id]
-    redirect_to root_url if @question.nil?
   end
 end
